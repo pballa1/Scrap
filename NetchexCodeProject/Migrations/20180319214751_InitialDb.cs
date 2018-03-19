59f98480de1be5f337a75c63cf1e806af427aa74
Patrick Ballard <pballa1@gmail.com>
2018-03-19 16:53:08 -0500

    Add migration scripts and edit tables for singularity

---
+++ NetchexCodeProject/Migrations/20180319214751_InitialDb.cs
@@ -0,0 +1,86 @@
+﻿using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using System;
+using System.Collections.Generic;
+
+namespace NetchexCodeProject.Migrations
+{
+    public partial class InitialDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "Employee",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    BirthDate = table.Column<string>(nullable: true),
+                    FirstName = table.Column<string>(nullable: true),
+                    LastName = table.Column<string>(nullable: true),
+                    MiddleName = table.Column<string>(nullable: true),
+                    SocialSecurityNumber = table.Column<string>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Employee", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PayFrequency",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false)
+                        .Annotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn),
+                    Name = table.Column<string>(nullable: true),
+                    PayPeriodsPerYear = table.Column<int>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_PayFrequency", x => x.Id);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "EmployeePay",
+                columns: table => new
+                {
+                    Id = table.Column<int>(nullable: false),
+                    EmployeeId = table.Column<int>(nullable: false),
+                    HireDate = table.Column<DateTime>(nullable: false),
+                    HourlyRate = table.Column<decimal>(nullable: false),
+                    IsSalary = table.Column<bool>(nullable: false),
+                    PayFrequencyId = table.Column<int>(nullable: false),
+                    PayPeriodAmount = table.Column<decimal>(nullable: false),
+                    Salary = table.Column<decimal>(nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_EmployeePay", x => x.Id);
+                    table.ForeignKey(
+                        name: "FK_EmployeePay_Employee_Id",
+                        column: x => x.Id,
+                        principalTable: "Employee",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_EmployeePay_PayFrequency_Id",
+                        column: x => x.Id,
+                        principalTable: "PayFrequency",
+                        principalColumn: "Id",
+                        onDelete: ReferentialAction.Cascade);
+                });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "EmployeePay");
+
+            migrationBuilder.DropTable(
+                name: "Employee");
+
+            migrationBuilder.DropTable(
+                name: "PayFrequency");
+        }
+    }
+}
