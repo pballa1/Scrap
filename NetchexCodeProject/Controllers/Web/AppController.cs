1981058c274d055b05c1e7d09976b60c2070d623
Patrick Ballard <pballa1@gmail.com>
2018-03-19 15:50:35 -0500

    Set up MVC

---
+++ NetchexCodeProject/Controllers/Web/AppController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace NetchexCodeProject.Controllers.Web
+{
+    public class AppController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
--- NetchexCodeProject/Startup.cs
+++ NetchexCodeProject/Startup.cs
@@ -1,20 +1,33 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 
 namespace NetchexCodeProject
 {
-    public class Startup
+	public class Startup
     {
-        // This method gets called by the runtime. Use this method to add services to the container.
+		private IHostingEnvironment _env;
+		private IConfigurationRoot _config;
+
+		public Startup(IHostingEnvironment env)
+		{
+			_env = env;
+
+			var builder = new ConfigurationBuilder()
+				.SetBasePath(_env.ContentRootPath)
+				.AddJsonFile("config.json")
+				.AddEnvironmentVariables();
+
+			_config = builder.Build();
+		}
+		
+		// This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+			services.AddSingleton(_config);
+			services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -25,10 +38,15 @@ namespace NetchexCodeProject
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
+			app.UseStaticFiles();
+			app.UseMvc(config =>
+			{
+				config.MapRoute(
+					name: "Default",
+					template: "{controller}/{action}/{id?}",
+					defaults: new { controller = "App", action = "Index" }
+				);
+			});
         }
     }
 }
